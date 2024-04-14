9f0aa020ab3a407cec5987cf6fa1f4dc8c6dabe5
Albert Shomakhov <shoma.albert081205@gmail.com>
2024-04-14 19:26:32 +0300

    14.04

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-nums = [int(i) for i in open('17-d3.txt')]
-
-cnt = 0
-max_sum = -10**11
-
-razmah = max(nums)-min(nums)
-
-for i in range(len(nums)-2):
-    num_1 = nums[i]
-    num_2 = nums[i+1]
-    num_3 = nums[i+2]
-    if num_1 < razmah and num_2 < razmah and num_3 < razmah:
-        if int('2' in num_1) + int('2' in num_2) + int('2' in num_3) >= 2:
-            cnt += 1
-            max_sum = max(max_sum, num_1+num_2+num_3)
+print(sorted('КАЛЕЙДСОП', reverse=True))
